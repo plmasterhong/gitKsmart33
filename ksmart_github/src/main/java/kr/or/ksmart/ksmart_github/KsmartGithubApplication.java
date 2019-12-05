5290a42187b09db303745d783965379171730e8a
Administrator <Administrator@teacher202>
2019-12-05 10:47:31 +0900

    [team1] ksmartproject git First step

---
+++ ksmart_github/src/main/java/kr/or/ksmart/ksmart_github/KsmartGithubApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.ksmart_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmartGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmartGithubApplication.class, args);
+	}
+
+}
---
+++ ksmart_github/src/test/java/kr/or/ksmart/ksmart_github/KsmartGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.ksmart_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmartGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
